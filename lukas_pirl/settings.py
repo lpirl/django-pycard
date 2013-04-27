fd895db9b631145cb45c51588ea4c06ef86a3288
Lukas Pirl <git@lukas-pirl.de>
2013-04-27 17:28:30 +0200

    altered MEDIA_URL 	fixme: download of uploads

--- lukas_pirl/settings.py
+++ lukas_pirl/settings.py
@@ -53,7 +53,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path_join(PROJECT_PATH, "../media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
