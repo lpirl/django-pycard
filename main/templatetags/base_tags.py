aa843ec43f5ad51788f4bf2372d550ccdb8768a5
Lukas Pirl <git@lukas-pirl.de>
2013-04-27 23:16:22 +0200

    added basic display of menu

--- main/templatetags/base_tags.py
+++ main/templatetags/base_tags.py
@@ -2,35 +2,39 @@ from random import randint
 
 from django import template
 
-from main.models import Configuration
+from main.models import Configuration, MenuItem
 
 register = template.Library()
 
 def generate_squares():
 
-	conf_get_int = Configuration.get_int
-	height_min = conf_get_int('squares_height_min')
-	height_max = conf_get_int('squares_height_max')
-	width_min = conf_get_int('squares_width_min')
-	width_max = conf_get_int('squares_width_max')
-
 	def square_properties_generator():
+
+		conf_get_int = Configuration.get_int
+		height_min = conf_get_int('squares_height_min')
+		height_max = conf_get_int('squares_height_max')
+		width_min = conf_get_int('squares_width_min')
+		width_max = conf_get_int('squares_width_max')
 
 		for _ in range(conf_get_int('squares_count')):
 
 			height = randint(height_min, height_max)
 			width = randint(width_min, width_max)
-			left = randint(0, 100) - width/2
-			top = randint(0, 100) - height/2
 
 			yield {
-				'height': height,
-				'width': width,
-				'top': top,
-				'left': left
+				'height':	height,
+				'width':	width,
+				'left':		randint(0, 100) - width/2,
+				'top':		randint(0, 100) - height/2
 			}
 
 	return {
 		'squares': square_properties_generator()
 		}
 register.inclusion_tag('squares.html')(generate_squares)
+
+def generate_menu():
+	return {
+		'items': MenuItem.objects.filter(root_article__hide=False)
+		}
+register.inclusion_tag('menu.html')(generate_menu)
