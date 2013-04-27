414141c8fe23f4957657a401ec6273bd443045d3
Lukas Pirl <git@lukas-pirl.de>
2013-04-27 18:38:52 +0200

    added model for configurations

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib.admin import site
 from django.contrib.admin import ModelAdmin
-from main.models import MenuItem, Article, Attachment
+from main.models import MenuItem, Article, Attachment, Configuration
 
 site.register(MenuItem, ModelAdmin)
 site.register(Article, ModelAdmin)
 site.register(Attachment, ModelAdmin)
+site.register(Configuration, ModelAdmin)
--- main/models.py
+++ main/models.py
@@ -2,6 +2,34 @@ from django.db.models import (	Model, IntegerField, ForeignKey,
 								CharField, BooleanField, FileField,
 								ManyToManyField, TextField)
 
+class Configuration(Model):
+	"""
+	Holds global options.
+	"""
+	option = CharField(	primary_key=True,
+						max_length=64,
+						null=False,
+						blank=False,
+						editable=True,
+						unique=True)
+	value = CharField(max_length=64, blank=True)
+	comment = TextField(blank=True)
+
+	@staticmethod
+	def get_str(option):
+		return Configuration.objects.get_or_create(option=option)[0].value
+
+	@staticmethod
+	def get_int(option):
+		return int(Configuration.get_str(option) or 0)
+
+	@staticmethod
+	def get_decimal(option):
+		return Decimal(Configuration.get_str(option) or 0)
+
+	def __unicode__(self):
+		return unicode('%s : %s' % (self.option, self.value))
+
 class MenuItem(Model):
 	pos_x = IntegerField(null=False, blank=False, editable=True)
 	pos_y = IntegerField(null=False, blank=False, editable=True)
