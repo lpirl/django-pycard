5cc17b73c534344a3f596690c037e9494a65b52a
Lukas Pirl <git@lukas-pirl.de>
2013-04-28 01:29:05 +0200

    added basic article view

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	url(r'^$', 'main.views.index', name='index'),
+	url(r'^$', 'main.views.index'),
+	url(r'^(.+)$', 'main.views.article'),
 )
--- main/views.py
+++ main/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from main.models import Article
 
 def index(request):
 	return render(
 		request,
 		'index.html',
 		{'vertically_center_menu': True}
+	)
+
+def article(request, slug):
+	return render(
+		request,
+		'article.html',
+		{
+			'article': get_object_or_404(Article, slug=slug),
+		}
 	)
 
