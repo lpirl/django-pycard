a8eb028d66f00218b99d11be2d57ded907345c00
Lukas Pirl <git@lukas-pirl.de>
2013-08-18 18:53:47 +0200

    changed widget for teaser to textarea

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,19 @@
 from django.contrib.admin import site, ModelAdmin
+from django.forms import ModelForm
+from django.forms.widgets import Textarea
 from main.models import (MenuItem, Article, Attachment, Configuration,
                          ContentMedia)
 
+from django.contrib import admin
+
+class ArticleAdminForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ArticleAdminForm, self).__init__(*args, **kwargs)
+        self.fields['teaser'].widget = admin.widgets.AdminTextareaWidget()
+
 class ArticleAdmin(ModelAdmin):
     list_display = ('__unicode__', 'teaser', 'parent', 'date_modified')
+    form = ArticleAdminForm
 
 site.register(MenuItem, ModelAdmin)
 site.register(Article, ArticleAdmin)
