82d740dae1997c72cd125e1bff386fa92455f066
Lukas Pirl <git@lukas-pirl.de>
2013-08-19 13:11:09 +0200

    added test for sub_articles_list and added docstrings

--- main/tests.py
+++ main/tests.py
@@ -4,6 +4,9 @@ from main.models import Article, Configuration, MenuItem
 from main.forms import ContactForm
 
 class ContactFormTest(TestCase):
+    """
+    Tests for the contact form
+    """
     fixtures = ["test_data/contact.json"]
     
     def setUp(self):
@@ -85,17 +88,29 @@ class ContactFormTest(TestCase):
                 self.assertTrue("unknown" in sender)
 
 class ArticleUrlTest(TestCase):
+    """
+    Tests for URLs pointing to articles.
+
+    This sis somehow interesting because this app resolves some URLs
+    itself.
+    """
     fixtures = ["test_data/article.json"]
     
     def setUp(self):
         self.valid_url = "/level1_1/level2_1/level3_1/?query=foo#section"
 
     def test_valid_url(self):
+        """
+        Make sure valid URLs (get_absolute_url()) works
+        """
         response = self.client.get(self.valid_url)
         self.assertTrue(Article.objects.get(slug="level2_1").hide)
         self.assertContains(response, "Level 3 Article 1")
 
     def assert_404_for_modified_valid_url(self, search, replace):
+        """
+        Shortcut for all checks expecting a 404 for a wrong URL
+        """
         url = self.valid_url.replace(search, replace)
         response = self.client.get(url)
         self.assertEqual(
@@ -105,15 +120,33 @@ class ArticleUrlTest(TestCase):
         )
 
     def test_missing_parent(self):
+        """
+        404 if not all parents are specified in the URL
+        """
         self.assert_404_for_modified_valid_url("/level2_1", "")
 
     def test_no_parents(self):
+        """
+        404 if only the slug (w/o any parents) is specified in the URL
+        """
         self.assert_404_for_modified_valid_url("/level1_1/level2_1", "")
 
     def test_wrong_parent(self):
+        """
+        404 if there is a not-parent (but existing article) in the URL
+        """
         self.assert_404_for_modified_valid_url("level2_1", "level2_2")
 
+class ArticleTemplateTest(TestCase):
+    """
+    Tests for the display of an article
+    """
+    fixtures = ["test_data/article.json"]
+
 class TagsTest(TestCase):
+    """
+    Tests for the templatetags module
+    """
     fixtures = ["test_data/article.json", "test_data/configuration.json",
                 "test_data/menu_items.json"]
     
@@ -184,4 +217,34 @@ class TagsTest(TestCase):
             self.assertContains(
                 self.response_index,
                 string
+            )
+
+    def test_subarticle_list_hide(self):
+        """
+        Tests if hidden articles are hidden
+        """
+        hidden_qs = Article.objects.filter(hide=True).exclude(parent=None)
+        self.assertTrue(hidden_qs.exists())
+        hidden = hidden_qs[0]
+        self.assertNotContains(
+            self.client.get(hidden.parents()[-1].get_absolute_url()),
+            hidden.headline
+        )
+
+    def test_subarticle_list(self):
+        """
+        Tests if sub articles are displayed
+        """
+        suparticle = Article.objects.exclude(hide=True).exclude(
+            parent=None)[0]
+
+        response = self.client.get(suparticle.get_absolute_url())
+        for subarticle in suparticle.children.all():
+            self.assertContains(
+                response,
+                subarticle.headline
+            )
+            self.assertContains(
+                response,
+                subarticle.teaser
             )
